afb1e06bce1cbf8a61d4716f4ea73cf79c213f9b
Cheelax <thomas.belloc@gmail.com>
2024-04-16 17:21:14 +0100

    add chakraUI

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import "./theme/fonts.css";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const theme = extendTheme({
+  fonts: {
+    body: "Chicago FLF, ui-sans-serif, system-ui",
+    heading: "Chicago FLF, ui-sans-serif, system-ui",
+  },
+  colors: {
+    green: "#00ff00",
+    custom_green: "#172217",
+  },
+  textColor: {
+    DEFAULT: "#00ff00",
+  },
+  styles: {
+    global: {
+      "html, body": {
+        color: "green", // Use the color key defined in the theme
+        backgroundColor: "custom_green", // Use the color key defined in the theme
+      },
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
