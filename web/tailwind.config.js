af182ec6c9bd8a3aa85231b58dc63394e9a0ab8c
Cheelax <thomas.belloc@gmail.com>
2024-04-16 15:55:06 +0100

    adding default text style as roll your own

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Chicago FLF", "ui-sans-serif", "system-ui"],
+      },
+      colors: {
+        green: "#00ff00", // Remplacez cette valeur par la teinte exacte de vert que vous voulez.
+      },
+      textColor: {
+        DEFAULT: "#00ff00", // Utilisez le même vert ici pour la couleur de texte par défaut.
+      },
+    },
   },
   plugins: [],
-}
-
+};
