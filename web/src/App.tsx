6a4b6f1370f9776d3ed729fe0f927d21444d5d8b
Rute Figueiredo <rfigueiredo.dev@gmail.com>
2024-04-17 12:30:11 +0100

    centering layout

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,4 @@
-import "./index.css";
-import "./theme/fonts.css";
+import React from "react";
 import DrugInventoryInfo from "./components/DrugInventoryInfo";
 import CharacterStats from "./components/CharacterStats";
 import { Map } from "./components/Map";
@@ -11,7 +10,6 @@ function App() {
     range: 5,
   };
 
-  // Example character data
   const characterData = {
     name: "Aragorn",
     str: 7,
@@ -25,21 +23,30 @@ function App() {
   };
 
   return (
-    <>
-      <div>
-        <Map width={10} height={10}></Map>
+    <div className="flex flex-col h-screen m-4">
+      <div className="flex flex-1 min-h-0">
+        {/* Map takes the majority of the screen */}
+        <div className="flex-grow relative p-2 bg-opacity-50">
+          <Map width={10} height={10} />
+        </div>
+        {/* Sidebar for character stats */}
+        <div className="w-1/4 min-w-min overflow-auto p-2 bg-opacity-50">
+          <CharacterStats characterData={characterData} />
+        </div>
       </div>
-      <div className="fixed right-0 top-0 h-full w-1/10 overflow-auto">
-        <CharacterStats characterData={characterData} />
-      </div>
-      <div className="fixed bottom-0 w-full flex justify-around items-center px-4 py-2">
-        <Button onClick={() => console.log("attack")}>Attack</Button>
-        <Button onClick={() => console.log("take drugs")}>Take Drugs</Button>
-        <div className="flex-1 w-1/4 h-1/10 overflow-auto">
+      {/* Bottom bar for actions and inventory */}
+      <div className="flex justify-between items-center p-2 fixed inset-x-0 bottom-0 bg-opacity-50">
+        {/* Buttons for actions */}
+        <div className="flex gap-4">
+          <Button onClick={() => console.log("attack")}>Attack</Button>
+          <Button onClick={() => console.log("take drugs")}>Take Drugs</Button>
+        </div>
+        {/* Flex container for inventory information */}
+        <div className="flex-1 flex justify-end">
           <DrugInventoryInfo />
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
